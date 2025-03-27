8c228f9532bede58c8619565f647521fb4ae0444
Andre <andre@tos.world>
2025-03-27 07:34:36 -0300

    fix: change setup to hook

--- src/email.service.ts
+++ src/email.service.ts
@@ -1,4 +1,12 @@
-import { Application, Config, Logger, Service, Singleton } from '@cmmv/core';
+import {
+    Application,
+    Config,
+    Logger,
+    Service,
+    Singleton,
+    Hook,
+    HooksType,
+} from '@cmmv/core';
 
 import * as nodemailer from 'nodemailer';
 import * as AWS from 'aws-sdk';
@@ -6,42 +14,50 @@ import Mail from 'nodemailer/lib/mailer';
 
 @Service('email')
 export class EmailService extends Singleton {
-  public logger: Logger = new Logger('EmailService');
-  public transporter: nodemailer.Transporter;
+    public logger: Logger = new Logger('EmailService');
+    public transporter: nodemailer.Transporter = null;
 
-  public static async loadConfig(application: Application): Promise<void> {
-    const instance = EmailService.getInstance();
-    const options = Config.get<any>('email', {});
+    @Hook(HooksType.onListen)
+    public async onListen(): Promise<void> {
+        const instance = EmailService.getInstance();
+        const options = Config.get<any>('email', {});
 
-    if (options?.SES) options.SES = new AWS.SES(options.SES);
+        if (options?.SES) options.SES = new AWS.SES(options.SES);
 
-    instance.transporter = nodemailer.createTransport(options);
-  }
+        instance.transporter = nodemailer.createTransport(options);
 
-  public async send(
-    from: string,
-    to: string,
-    subject: string,
-    html: string,
-    options?: Mail.Options,
-  ): Promise<boolean> {
-    const instance = EmailService.getInstance();
-
-    try {
-      const optionsLogger = Config.get<boolean>('email.logger', false);
-      let message: Mail.Options = { from, to, subject, html };
-
-      if (options) message = { ...message, ...options };
-
-      const info = await instance.transporter.sendMail(message);
-
-      if (optionsLogger)
-        instance.logger.log(`Message sent to ${to}: ${info.messageId}`);
-
-      return true;
-    } catch (err) {
-      instance.logger.error(`Error sending email to ${to}: ${err.message}`);
-      return false;
+        this.logger.log('EmailService is listening');
     }
-  }
+
+    public async send(
+        from: string,
+        to: string,
+        subject: string,
+        html: string,
+        options?: Mail.Options,
+    ): Promise<boolean> {
+        const instance = EmailService.getInstance();
+
+        if (!instance.transporter)
+            throw new Error('EmailService is not listening');
+
+        try {
+            const optionsLogger = Config.get<boolean>('email.logger', false);
+            let message: Mail.Options = { from, to, subject, html };
+
+            if (options) message = { ...message, ...options };
+
+            const info = await instance.transporter.sendMail(message);
+
+            if (optionsLogger)
+                instance.logger.log(`Message sent to ${to}: ${info.messageId}`);
+
+            return true;
+        } catch (err) {
+            instance.logger.error(
+                `Error sending email to ${to}: ${err.message}`,
+            );
+            return false;
+        }
+    }
 }
